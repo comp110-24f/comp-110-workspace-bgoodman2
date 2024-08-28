14f75a4a191ed48c62458cc3f154fcc7c01c243c
bgoodman2 <bgoodman@unc.edu>
2024-08-28 09:36:44 -0400

    CQ00 finished

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,19 @@
+"""Functions Challenge Question"""
+
+__author__ = "730475190"
+
+
+def mimic(message: str) -> str:
+    """should return the message you type in back to you"""
+    return message
+
+
+def main() -> None:
+    """wont print anything"""
+    return none
+
+
+print(mimic(message=input("What is your message")))
+
+if __name__ == "__main__":
+    main()
